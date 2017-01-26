b914c58fd73f12c083f518f7388e5b64b5ecdf1d
Gulshan Kumar <satasalamaa@gmail.com>
2017-01-26 13:46:14 +0200

    Java-SE-8-II-Certification-Practice
    Java SE 8 II Certification practice

---
+++ src/datatypes/Datatypes.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package datatypes;
+
+/**
+ * @author Gulshan Kumar
+ *
+ */
+public class Datatypes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Print Hello World
+		
+		System.out.println("Hello World!");
+		
+
+	}
+
+}
